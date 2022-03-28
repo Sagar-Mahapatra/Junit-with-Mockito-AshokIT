93f2354dd2148d913306980114c829cfceb48f38
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-03-28 11:09:31 +0530

    initial commit

---
+++ src/main/java/com/ashokit/JunitWithMockitoAshokItApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitWithMockitoAshokItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitWithMockitoAshokItApplication.class, args);
+	}
+
+}
